e8168d5007b493112adc784028efd3f2c6b77de6
Leonardo Kaplan <contato.leonardokaplan@gmail.com>
2015-03-21 18:50:52 -0300

    path configs

--- template/unix_devel/config.sh
+++ template/unix_devel/config.sh
@@ -4,4 +4,7 @@ cp -rf ./headers ../
 mkdir -p ../bin
 mkdir -p ../obj
 mkdir -p ../lib
-cp -f ./libArcaboucoTeste.a ../lib/
+cd ../../arcaboucounix/arcabouc/Fontes/
+make
+cd -
+cp -f ../../arcaboucounix/arcabouc/Fontes/libArcaboucoTeste.a ../lib/
